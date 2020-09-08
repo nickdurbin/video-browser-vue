d9d57bcfa131cf502534679cd56cf77ebef56b40
durbcodes <nick.durbin@gmail.com>
2020-09-08 14:59:33 -0400

    Created a basic app file to render the application.

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+const API_KEY = 'AIzaSyAs4WeYLBiD7lWi0D_79jyoFboEJeedZmQ'
+
+export default {
+  name: 'App',
+  components: {
+    SearchBar,
+    VideoList
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios.get('https://www.googlesapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err, 'Error'))
+    }
+  }      
+  };
+</script>

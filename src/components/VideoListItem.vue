d2867c4866662c098549c5384d327a238b08c799
nick.durbin <nick.durbin@gmail.com>
2020-09-09 11:08:33 -0400

    Created a new component for each individual item in the list of videos.

---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,38 @@
+<template>
+<li class="list-group-item media" @click="onVideoSelect">
+  <img class="mr-3" :src="thumbnailUrl" />
+  <div class="media-body">
+    {{ video.snippet.title }}
+  </div>
+</li>
+</template>
+
+<script>
+export default {
+  name: 'VideoListItem',
+  props: ['video'],
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    },
+  },
+  methods: {
+    onVideoSelect() {
+      this.$emit('videoSelect', this.video);
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  cursor: pointer;
+  background: #28262e;
+  color: #dfdee2;
+}
+
+li:hover {
+  background-color: #3e3b46;
+}
+</style>

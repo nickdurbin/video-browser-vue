02a987ada7bcfc1d8aae33232a4c0cefde637bd8
nick.durbin <nick.durbin@gmail.com>
2020-09-09 11:07:51 -0400

    Made some styling changes to the search bar.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,21 +1,38 @@
 <template>
-  <div>
-    <input @input="onInput" />
-  </div>
+<div>
+  <input @input="onInput" placeholder="Search for content..." />
+</div>
 </template>
 
 <script>
 export default {
   name: 'SearchBar',
   methods: {
-    onInput: function(event) {
-      this.$emit('termChange', event.target.value)
-    }
-  }
-  
-}
+    onInput: function (event) {
+      this.$emit('termChange', event.target.value);
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+input {
+  width: 60%;
+  height: 3rem;
+  padding-left: 2%;
+  border-radius: 10px;
+  border: none;
+  background: #3e3b46;
+  color: #dfdee2;
+}
 
+input:focus {
+  outline: #4affa8;
+}
+
+div {
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+}
 </style>

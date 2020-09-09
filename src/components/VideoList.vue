2601ea77e83fab03fc15a50fe952d9388b807718
nick.durbin <nick.durbin@gmail.com>
2020-09-09 11:08:12 -0400

    Fixed some issues with the v-for and the item list.

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,19 +1,22 @@
 <template>
-  <ul>
-   Video List 
-   {{videos.length}}
-  </ul>
+<ul class="list-group">
+  <VideoListItem v-for="video in videos" :key="video.etag" :video="video">
+  </VideoListItem>
+</ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
+
 export default {
   name: 'VideoList',
+  components: {
+    VideoListItem,
+  },
   props: {
-    videos: Array
-  }
-}
+    videos: Array,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
